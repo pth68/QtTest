4b734ba414c7a1bcf9dcebdf8df3f26361920cdd
Peter Thoma <peter_thoma@outlook.com>
2018-11-23 19:28:49 +0100

    added 3d view

--- QtGuiApplication1/qtguiapplication1.cpp
+++ QtGuiApplication1/qtguiapplication1.cpp
@@ -5,6 +5,14 @@ QtGuiApplication1::QtGuiApplication1(QWidget *parent)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
+
+	QListWidgetItem *item1 = new QListWidgetItem("test roter Hintergrund", ui.myList);
+	item1->setBackground(QBrush(QColor(255, 0, 0)));
+
+	QListWidgetItem *item2 = new QListWidgetItem("test normal", ui.myList);
+
+	ui.myList->setCurrentItem(item2);
+
 }
 
 void QtGuiApplication1::on_okButton_clicked()
